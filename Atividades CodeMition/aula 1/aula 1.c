e434226ebb81c6534572e648ecb86ead21a7279a
KolAndre <andreluisribeiro948@gmail.com>
2026-05-28 12:06:56 -0300

    Create aula 1.c

---
+++ Atividades CodeMition/aula 1/aula 1.c
@@ -0,0 +1,11 @@
+# include  <stdio.h>
+int main () {
+	
+	int x;
+	
+	scanf ("%d", &x);
+	
+	printf ( " seu numero antecessor seria: %d \n", x - 1);
+	
+	printf ( " seu numero sucessor seria: %d \n", x + 1);
+}
